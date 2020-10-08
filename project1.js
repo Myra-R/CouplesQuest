2802f8b48532a6913f6abc9e3d6769b5b1b671e5
Wendy Grant <wendykgrant.tx@gmail.com>
2020-10-08 10:12:47 -0500

    Added weather div and js updates

--- project1.js
+++ project1.js
@@ -1,100 +1,48 @@
-let city = "";
-
-let citiesDiv = document.getElementById("favorite_cities");
-//start with empty array
-
-let cities = [];
-init();
-listClicker();
-searchClicker();
-
-// pull search history from local storage if available
-function init() {
-    let saved_cities = JSON.parse(localStorage.getItem("cities"));
-
-    if (saved_cities !== null) {
-        cities = saved_cities
-    }
-
-    renderButtons();
-}
-
-// assign search history to local storage
-function storeCities() {
-    localStorage.setItem("cities", JSON.stringify(cities));
-}
-
-
-//render buttons for each element in cities array as a search history for user
-
-function renderButtons() {
-    citiesDiv.innerHTML = "";
-    if (cities == null) {
-        return;
-    }
-    let new_cities = [...new Set(cities)];
-    for (let i = 0; i < new_cities.length; i++) {
-        let cityName = new_cities[i];
-
-        let buttonEl = document.createElement("button");
-        buttonEl.textContent = cityName;
-        buttonEl.setAttribute("class", "listbtn");
-
-        citiesDiv.appendChild(buttonEl);
-        listClicker();
-    }
-}
-//on click function for search history buttons
-function listClicker() {
-    $(".listbtn").on("click", function (event) {
-        event.preventDefault();
-        city = $(this).text().trim();
-        APIcalls();
-    })
-}
-
-
-
-// click event for search button
-function searchClicker() {
-    $("#searchbtn").on("click", function (event) {
-        event.preventDefault();
-        city = $(this).prev().val().trim()
-
-        //push the city into favorites array
-        cities.push(city);
-
-        //max array.length is 10, if over 10 the newest search will be saved & the oldest discarded
-        if (cities.length > 10) {
-            cities.shift()
-        }
-        // if there are no saved search results
-        if (city == "") {
-            return;
-        }
-        APIcalls();
-        storeCities();
-        renderButtons();
-
-        // "https://api.openweathermap.org/data/2.5/weather?q=houston&appid=a46d7ace8fe9b30fe73ee26488f40d18",
-    })
-}
-
+var apiKey = "5ce8439fd4264478d1da0b24a7cd547d";
+var city = "";
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a46d7ace8fe9b30fe73ee26488f40d18";
-
-
-    $.ajax({
-        url = queryURL,
-        method: "GET",
-    }).then(function (current_data) {
-        console.log(current_data);
-        let temp = Math.round(((current_data.main.temp - 273.15) * 9 / 5 + 32))
-        console.log("The temperature in " + city + " is: " + temp);
-        $("#temp").text("Temperature: " + temp + String.fromCharCode(176) + "F");
-        $("#hum").text("Humidity: " + current_data.main.humidity + " %");
-        $("#c_wind").text("Wind Speed: " + current_data.wind.speed + " MPH");
-        $("#weather_icon").attr({
-            "src": "http://openweathermap.org/img/w/" + current_data.weather[0].icon + ".png",
-            "height": "100px", "width": "100px"
-        });
-    });
+ 
+$(document).ready(function() {
+    // geolocation enabled
+  
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(showcityname);
+  
+      function showcityname(position) {
+        var lat = position.coords.latitude;
+        var longit = position.coords.longitude;
+        var altitude = position.coords.altitude;
+        var latitude_text = document.getElementById("latitude-val");
+        var altitude_text = document.getElementById("altit");
+        var city_name;
+        var temp;
+        var pressure;
+        var wind_speed;
+        var country_name;
+        var weather_description;
+        var apiKey = "5dd765a29b95b2e058dfd9f33a1dbd0d";
+  
+        altitude_text.innerHTML = "Altitude is " + altitude;
+        latitude_text.innerHTML = "Latitude is " + lat;
+  
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + longit + "&appid=" + apiKey, function(data) {
+  
+          city_name = data["name"];
+          country_name = data["sys"]["country"];
+          weather_description = data["weather"][0]["description"];
+          temp = data["main"]["temp"];
+          pressure = data["main"]["pressure"];
+          wind_speed = data["wind"]["speed"];
+  
+          $("#cityname").html(city_name + " &#40;" + country_name + "&#41; " + "has " + weather_description);
+          $(".temp").html(temp);
+          $(".pressure").html(pressure + " mBar");
+          $(".wind-spd").html(wind_speed + " m/s");
+  
+        })
+  
+      }
+  
+    }
+  
+  })

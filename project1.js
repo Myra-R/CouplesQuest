0fb1e0b634bb6e5be97529054ef77dd9924feb1c
Wendy Grant <wendykgrant.tx@gmail.com>
2020-10-08 14:03:29 -0500

    Weather js functioning now

--- project1.js
+++ project1.js
@@ -1,93 +1,26 @@
-let city = "";
-var apiKey = "5ce8439fd4264478d1da0b24a7cd547d";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a46d7ace8fe9b30fe73ee26488f40d18";
 
-//on click function for search history buttons
-function listClicker() {
-  $(".listbtn").on("click", function (event) {
-    event.preventDefault();
-    city = $(this).text().trim();
-    APIcalls();
-  })
-}
+$( "#searchbtn" ).on("click",function(){
 
-function searchClicker() {
-  $("#search").on("click", function (event) {
-    event.preventDefault();
-
-    // APIcalls();
-    storeCities();
-    renderButtons();
-  })
-}
+  let city = $(".inputValue").val();
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a46d7ace8fe9b30fe73ee26488f40d18";
 
 
-//     // get current conditions from openweather
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (current_data) {
-  console.log(current_data);
-  let temp = Math.round(((current_data.main.temp - 273.15) * 9 / 5 + 32))
-  // console.log("The temperature in " + city + " is: " + temp);
-  $(".current_temp").text("Temperature: " + temp + String.fromCharCode(176) + "F");
-  $(".current_hum").text("Humidity: " + current_data.main.humidity + " %");
-  $(".current_wind").text("Wind Speed: " + current_data.wind.speed + " MPH");
-  $(".current_weather_icon").attr({
-    "src": "http://openweathermap.org/img/w/" + current_data.weather[0].icon + ".png",
-    "height": "100px", "width": "100px"
+
+    // get current conditions from openweather
+    $.ajax({
+      url: weatherURL,
+      method: "GET",
+    }).then(function (current_data) {
+      console.log(current_data);
+      let temp = Math.round(((current_data.main.temp - 273.15) * 9 / 5 + 32))
+      console.log("The temperature in " + weatherCity + " is: " + temp);
+      $(".current_temp").text("Temperature: " + temp + String.fromCharCode(176) + "F");
+      $(".current_hum").text("Humidity: " + current_data.main.humidity + " %");
+      $(".current_wind").text("Wind Speed: " + current_data.wind.speed + " MPH");
+      $(".current_weather_icon").attr({
+        "src": "http://openweathermap.org/img/w/" + current_data.weather[0].icon + ".png",
+        "height": "100px", "width": "100px"
+      });
+    });
   });
 
-})
-
-
-  // function APIcalls() {
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  // }).then(function (response) {
-  // console.log(queryURL);
-  // console.log(response);
-
-  // let temp = Math.round(((current_data.main.temp - 273.15) * 9 / 5 + 32))
-  // console.log("The temperature in " + city + " is: " + temp);
-  // $(".current_temp").text("Temperature: " + temp + String.fromCharCode(176) + "F");
-  // $(".current_hum").text("Humidity: " + current_data.main.humidity + " %");
-  // $(".current_wind").text("Wind Speed: " + current_data.wind.speed + " MPH");
-  // $(".current_weather_icon").attr({
-  //   "src": "http://openweathermap.org/img/w/" + current_data.weather[0].icon + ".png",
-  //   "height": "100px", "width": "100px"
-  // });
-
-
-// }
-
-// button.addEventListener('click', function () {
-//     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&units=imperial&appid=a46d7ace8fe9b30fe73ee26488f40d18')
-//     // promises
-//     .then(response => response.json())
-//     .then(data => {
-//         var nameValue = data['name'];
-//         var tempValue = data['main']['temp'];
-//         var descValue = data['weather'][0]['description'];
-
-//         name.innerHTML = nameValue;
-//         desc.innerHTML = descValue;
-//         temp.innerHTML = tempValue;
-
-<<<<<<< HEAD
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-=======
-
->>>>>>> 7dd7cd1c9e1b2cc953fb5a2553b8f4ea08615580
